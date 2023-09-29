06feb6fbe07aafbf6c9d88f8f1c65626efe57ab1
Trevor Taubitz <terrabitz@protonmail.com>
2023-09-29 11:50:42 -0400

    fix: Add test change
    Signed-off-by: Trevor Taubitz <terrabitz@protonmail.com>

--- isatty_tcgets.go
+++ isatty_tcgets.go
@@ -4,11 +4,16 @@
 
 package isatty
 
-import "golang.org/x/sys/unix"
+import (
+	"fmt"
+
+	"golang.org/x/sys/unix"
+)
 
 // IsTerminal return true if the file descriptor is terminal.
 func IsTerminal(fd uintptr) bool {
 	_, err := unix.IoctlGetTermios(int(fd), unix.TCGETS)
+	fmt.Println("Test change")
 	return err == nil
 }
 
